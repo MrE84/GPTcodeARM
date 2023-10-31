98c1da648ea7a4f6741313e608e26106e7dff21b
MrE84 <135212999+MrE84@users.noreply.github.com>
2023-10-31 20:05:58 +0000

    with bt

--- testServo_modified_v2.4/testServo_modified_v2.4.ino
+++ testServo_modified_v2.4/testServo_modified_v2.4.ino
@@ -1,6 +1,5 @@
 #include <Wire.h> //Library which allows I2C communication.
 #include <Adafruit_PWMServoDriver.h> //This library must be downloaded to run the code.
-#include <SoftwareSerial.h>
  
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();//Instantiating objects with the Adafruit_PWMServoDriver class.
  
@@ -22,65 +21,19 @@ Serial.begin(9600);
 //Serial.println("16 channel Servo test!");
 pwm.begin();  //Initialize the library and send PWM signals.
 pwm.setPWMFreq(FREQUENCY); //Servo's update frequency at 60 Hertz.
-pwm.setPWM(1,0,pulseWidth(120)); //HIP_SERVO 1,
-pwm.setPWM(2,0,pulseWidth(120)); //WAAAIST_SERVO
-pwm.setPWM(3,0,pulseWidth(97)); //SHOULDER_SERVO
-pwm.setPWM(4,0,pulseWidth(88)); //ELBOW_SERVO 
+// pwm.setPWM(1,0,pulseWidth(120)); //HIP_SERVO 1,
+pwm.setPWM(2,0,pulseWidth(126)); //WAAAIST_SERVO
+pwm.setPWM(3,0,pulseWidth(85)); //SHOULDER_SERVO
+pwm.setPWM(4,0,pulseWidth(90)); //ELBOW_SERVO 
 pwm.setPWM(5,0,pulseWidth(0)); //WRIST_SERVO
 pwm.setPWM(6,0,pulseWidth(67)); //CRAW_SERVO
 }
  
 
-// pwm.setPWM(1,0,pulseWidth(60)); // HIP_SERVO 0, RANGE LEFT 0
-// delay(1000);
-// pwm.setPWM(1,0,pulseWidth(67));// HIP_SERVO 0, RANGE LEFT 180
-// delay(1500);
-// pwm.setPWM(1,0,pulseWidth(125));// HIP_SERVO 0, RANGE LEFT 90
-// delay(1000);
-
-// pwm.setPWM(2,0,pulseWidth(60)); // WAIST_SERVO, left
-// delay(2000);
-// pwm.setPWM(2,0,pulseWidth(180));// WAIST_SERVO, right 
-// delay(2000);
-// pwm.setPWM(2,0,pulseWidth(126));// WAIST_SERVO, centre
-// delay(2000);
-
-// pwm.setPWM(3,0,pulseWidth(0)); //SHOULDER_SERVO  left
-// delay(5000);
-// pwm.setPWM(3,0,pulseWidth(85));// SHOULDER_SERVO center
-// delay(5000);
-// pwm.setPWM(3,0,pulseWidth(180));// SHOULDER_SERVO right
-// delay(5000);
-
-// pwm.setPWM(4,0,pulseWidth(0)); //ELBOW_SERVO right
-// delay(5000);
-// pwm.setPWM(4,0,pulseWidth(90)); //ELBOW_SERVO centre
-// delay(2000);
-// pwm.setPWM(4,0,pulseWidth(180)); //ELBOW_SERVO left
-// delay(5000);
-
-// pwm.setPWM(5,0,pulseWidth(0)); //WRIST_SERVO flat center
-// delay(5000);
-// pwm.setPWM(5,0,pulseWidth(90)); //WRIST_SERVO vertical
-// delay(2000);
-// pwm.setPWM(5,0,pulseWidth(180)); //WRIST_SERVO 180 rotation
-// delay(5000);
-
-
-// pwm.setPWM(6,0,pulseWidth(67)); //Open claw.
-// delay(2000);
-// pwm.setPWM(6,0,pulseWidth(125)); //Close claw.
-// delay(3000);
-
-
- 
-
-
 void loop() {
   if (Serial.available()) { // Check if data is available on the serial port
     String command = Serial.readStringUntil('\n'); // Read the command
     executeCommand(command);
-    
   }
 
 
@@ -91,12 +44,10 @@ void executeCommand(String command) {
     int angle = command.substring(4).toInt();
     pwm.setPWM(1, 0, pulseWidth(angle));
     Serial.println("HIP moved to " + String(angle) + " degrees");
-    
   } else if (command.startsWith("WAAAIST ")) {
     int angle = command.substring(8).toInt();
     pwm.setPWM(2, 0, pulseWidth(angle));
     Serial.println("WAAAIST moved to " + String(angle) + " degrees");
-    
   } else if (command.startsWith("SHOULDER ")) {
     int angle = command.substring(9).toInt();
     pwm.setPWM(3, 0, pulseWidth(angle));
--- testServo_modified_v2_5/testServo_modified_v2_5.ino
+++ testServo_modified_v2_5/testServo_modified_v2_5.ino
@@ -1,9 +1,13 @@
+//https://srituhobby.com/how-to-make-a-bluetooth-controlled-robot-arm-with-arduino/
+
 #include <Wire.h> //Library which allows I2C communication.
 #include <Adafruit_PWMServoDriver.h> //This library must be downloaded to run the code.
 #include <SoftwareSerial.h>
  
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();//Instantiating objects with the Adafruit_PWMServoDriver class.
- 
+//SoftwareSerial getValue(2,3); // TX , RX
+SoftwareSerial bt(2,3); // TX , RX
+
 #define MIN_PULSE_WIDTH 500   //These are the minimum and maximum wavelength values which serve MG 995.
 #define MAX_PULSE_WIDTH 2500
 #define DEFAULT_PULSE_WIDTH 1500
@@ -19,7 +23,10 @@ return analog_value; //The value this function returns.
   
 void setup(){
 Serial.begin(9600);
+//getValue.begin(9600);
+bt.begin(9600);	/* Define baud rate for software serial communication */
 //Serial.println("16 channel Servo test!");
+
 pwm.begin();  //Initialize the library and send PWM signals.
 pwm.setPWMFreq(FREQUENCY); //Servo's update frequency at 60 Hertz.
 pwm.setPWM(1,0,pulseWidth(120)); //HIP_SERVO 1,
@@ -30,6 +37,67 @@ pwm.setPWM(5,0,pulseWidth(0)); //WRIST_SERVO
 pwm.setPWM(6,0,pulseWidth(67)); //CRAW_SERVO
 }
  
+
+
+
+ 
+
+
+void loop() {
+  if (Serial.available()) { // Check if data is available on the serial port
+    String command = Serial.readStringUntil('\n'); // Read the command
+    executeCommand(command);    
+    
+  }
+  if (bt.available()) { // Check if data is available on the serial port
+  Serial.write(bt.read());	/* Print character received on to the serial monitor */
+    String command = Serial.readStringUntil('\n'); // Read the command
+    executeCommand(command);    
+    
+  }
+
+
+
+//   if (getValue.available() > 0) { // Check if data is available on the serial port
+//    String command = getValue.readStringUntil('\n'); // Read the command
+//    executeCommand(command);
+//  }
+
+}
+
+void executeCommand(String command) {
+  if (command.startsWith("HIP ")) {
+    int angle = command.substring(4).toInt();
+    pwm.setPWM(1, 0, pulseWidth(angle));
+    Serial.println("HIP moved to " + String(angle) + " degrees");
+    
+  } else if (command.startsWith("WAAAIST ")) {
+    int angle = command.substring(8).toInt();
+    pwm.setPWM(2, 0, pulseWidth(angle));
+    Serial.println("WAAAIST moved to " + String(angle) + " degrees");
+    
+  } else if (command.startsWith("SHOULDER ")) {
+    int angle = command.substring(9).toInt();
+    pwm.setPWM(3, 0, pulseWidth(angle));
+    Serial.println("SHOULDER moved to " + String(angle) + " degrees");
+  } else if (command.startsWith("ELBOW ")) {
+    int angle = command.substring(6).toInt();
+    pwm.setPWM(4, 0, pulseWidth(angle));
+    Serial.println("ELBOW moved to " + String(angle) + " degrees");
+  } else if (command.startsWith("WRIST ")) {
+    int angle = command.substring(6).toInt();
+    pwm.setPWM(5, 0, pulseWidth(angle));
+    Serial.println("WRIST moved to " + String(angle) + " degrees");
+  } else if (command.startsWith("CRAW ")) {
+    int angle = command.substring(5).toInt();
+    pwm.setPWM(6, 0, pulseWidth(angle));
+    Serial.println("CRAW moved to " + String(angle) + " degrees");
+  } else {
+    Serial.println("Unknown command: " + command);
+  }
+}
+
+
 
 // pwm.setPWM(1,0,pulseWidth(60)); // HIP_SERVO 0, RANGE LEFT 0
 // delay(1000);
@@ -71,49 +139,3 @@ pwm.setPWM(6,0,pulseWidth(67)); //CRAW_SERVO
 // delay(2000);
 // pwm.setPWM(6,0,pulseWidth(125)); //Close claw.
 // delay(3000);
-
-
- 
-
-
-void loop() {
-  if (Serial.available()) { // Check if data is available on the serial port
-    String command = Serial.readStringUntil('\n'); // Read the command
-    executeCommand(command);
-    
-  }
-
-
-}
-
-void executeCommand(String command) {
-  if (command.startsWith("HIP ")) {
-    int angle = command.substring(4).toInt();
-    pwm.setPWM(1, 0, pulseWidth(angle));
-    Serial.println("HIP moved to " + String(angle) + " degrees");
-    
-  } else if (command.startsWith("WAAAIST ")) {
-    int angle = command.substring(8).toInt();
-    pwm.setPWM(2, 0, pulseWidth(angle));
-    Serial.println("WAAAIST moved to " + String(angle) + " degrees");
-    
-  } else if (command.startsWith("SHOULDER ")) {
-    int angle = command.substring(9).toInt();
-    pwm.setPWM(3, 0, pulseWidth(angle));
-    Serial.println("SHOULDER moved to " + String(angle) + " degrees");
-  } else if (command.startsWith("ELBOW ")) {
-    int angle = command.substring(6).toInt();
-    pwm.setPWM(4, 0, pulseWidth(angle));
-    Serial.println("ELBOW moved to " + String(angle) + " degrees");
-  } else if (command.startsWith("WRIST ")) {
-    int angle = command.substring(6).toInt();
-    pwm.setPWM(5, 0, pulseWidth(angle));
-    Serial.println("WRIST moved to " + String(angle) + " degrees");
-  } else if (command.startsWith("CRAW ")) {
-    int angle = command.substring(5).toInt();
-    pwm.setPWM(6, 0, pulseWidth(angle));
-    Serial.println("CRAW moved to " + String(angle) + " degrees");
-  } else {
-    Serial.println("Unknown command: " + command);
-  }
-}
